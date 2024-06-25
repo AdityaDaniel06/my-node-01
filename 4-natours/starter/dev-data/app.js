2d84748f20a6fbb5555b1a26b3e79e3d20306ef9
AdityaDaniel06 <danieladitya327@gmail.com>
2024-06-25 16:15:09 +0530

    first commit

---
+++ 4-natours/starter/dev-data/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+// define routes
+app.get('/', (req, res) => {
+  // res.status(200).send('Hello from the server side.');
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side.', app: 'natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('Sending Post request...');
+});
+// to start a server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
